f048f7f2f4a32ba85a83a3bd39602bac5026684e
Phil Labrum <phil@altir.app>
2024-01-14 02:01:43 -0500

    added icons and basic tkinter

--- app.py
+++ app.py
@@ -1,21 +1,49 @@
 import tkinter as tk
+from tkinter import filedialog
 
 
-def on_button_click():
-    label.config(text="Hello, Tkinter!")
+def submit_files(file1, file2):
+    print("File 1:", file1.get())
+    print("File 2:", file2.get())
 
 
 # Create the main window
-app = tk.Tk()
-app.title("Minimal Tkinter App")
+root = tk.Tk()
+root.title("Master Processor")
+root.geometry("800x500")
 
-# Create a label
-label = tk.Label(app, text="Welcome to Tkinter!")
-label.pack(pady=10)
 
-# Create a button
-button = tk.Button(app, text="Click me!", command=on_button_click)
-button.pack(pady=10)
+# Variables to store selected file paths
+master_file = tk.StringVar(value="no master file selected")
+lastest_export_file = tk.StringVar(value="no export file selected")
+
+
+def browse_master_file():
+    file_path = filedialog.askopenfilename()
+    master_file.set(file_path)
+    print("selected master file", master_file.get())
+    l1.configure(text=master_file.get())
+
+
+def browse_export_file():
+    file_path = filedialog.askopenfilename()
+    lastest_export_file.set(file_path)
+    print("selected file", lastest_export_file.get())
+    l2.configure(text=lastest_export_file.get())
+
+
+# Entry widgets to display selected file paths
+# Button to open the file browser
+master_file_button = tk.Button(root, text="Select Master File", command=browse_master_file)
+master_file_button.pack()
+l1 = tk.Label(root, text=(master_file.get()))
+l1.pack()
+
+latest_export_button = tk.Button(root, text="Select Latest Export", command=browse_export_file)
+latest_export_button.pack()
+l2 = tk.Label(root, text=(lastest_export_file.get()))
+l2.pack()
+
 
 # Run the Tkinter event loop
-app.mainloop()
+root.mainloop()
